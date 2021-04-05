b1afb28d514e2fe0d0282d217d9bd0d5da363d29
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-05 23:40:25 +0200

    JS_Exercice_P1_2

---
+++ main.js
@@ -0,0 +1,17 @@
+alert('Nom\nPrénom\nVille');
+
+// ou bien:
+// alert('Nom' + '\n' +  'Prenom' + '\n' + 'Ville');  
+
+// ou bien:
+// var nom ='Francois';
+// var prénom ='flavie';
+// var ville = 'Paris';
+
+// var res = 'Nom :' + nom + ' ' + 'prénom :' + prénom + ' ' + 'ville:' + ville;
+// var res = 'Nom :' + nom + '\n' + 'prénom :' + prénom + '\n' + 'ville:' + ville;
+
+// pour l'éspace dans une variable  Aire sur l'Adour
+// exemple: 
+// var ville = 'Aire sur l'Adour'
+// var ville = 'Aire sur l\'Adour'
